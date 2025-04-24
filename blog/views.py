899d0676949f2674c42f32941ad03eeb281b7904
Nicolas <nicolasmbertin@gmail.com>
2025-04-24 14:39:29 -0300

    Actualización: Mejoras en los templates y funcionalidad de búsqueda

--- blog/views.py
+++ blog/views.py
@@ -20,10 +20,12 @@ def agregar_jugador(request):
     return render(request, 'blog/form_jugador.html', {'formulario': formulario})
 
 def buscar_jugadores(request):
-    jugadores = Jugador.objects.all()
-    return render(request, 'blog/jugadores.html', {'jugadores': jugadores})
-
-
+    query = request.GET.get('q')  # Obtiene el valor del campo de búsqueda
+    if query:
+        jugadores = Jugador.objects.filter(nombre__icontains=query)  # Filtra jugadores por nombre
+    else:
+        jugadores = Jugador.objects.all()  # Muestra todos los jugadores si no hay búsqueda
+    return render(request, 'blog/jugadores.html', {'jugadores': jugadores, 'query': query})
 
 
     
